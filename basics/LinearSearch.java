020657ea94bc2aad7711451fe106303655ac07f5
Raghuraj Singh <raghurajsingh152007@gmail.com>
2026-09-09 22:48:28 +0530

    Implement main method for Linear Search with user input handling

--- basics/LinearSearch.java
+++ basics/LinearSearch.java
@@ -1,13 +1,44 @@
-package basics;
+import java.util.Scanner;
 
 public class LinearSearch {
-    public static int linearSearch(int[] arr, int target) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == target) {
-                return i; // Return the index of the target element
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Taking array size from user
+        System.out.print("Enter the size of array: ");
+        int x = sc.nextInt();
+
+        int[] numbers = new int[x];
+
+        // Taking array elements from user
+        System.out.println("Enter " + x + " elements:");
+
+        for (int i = 0; i < x; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        // Taking the number to search
+        System.out.print("Enter the number to search: ");
+        int target = sc.nextInt();
+
+        // Linear Search
+        int size = numbers.length;
+        boolean found = false;
+
+        for (int i = 0; i < size; i++) {
+
+            if (numbers[i] == target) {
+                System.out.println("Element found at index: " + i);
+                found = true;
+                break;
             }
         }
-        return -1; // Return -1 if the target element is not found
-    }
 
+        if (!found) {
+            System.out.println("Element not found");
+        }
+
+        sc.close();
+    }
 }
