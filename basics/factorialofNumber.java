18394c6c942714ceed9cc6faad26ace180d8922b
Raghuraj Singh <raghurajsingh152007@gmail.com>
2026-09-09 21:56:52 +0530

    Add subtract method and implement factorial calculation

---
+++ basics/factorialofNumber.java
@@ -0,0 +1,15 @@
+package basics;
+
+public class factorialofNumber {
+    public static void main(String[] args) {
+        int number = 5; // Number to calculate factorial
+        long factorial = 1; // Variable to store factorial result
+
+        for (int i = 1; i <= number; i++) {
+            factorial *= i; // Multiply factorial by current number
+        }
+
+        System.out.println("The factorial of " + number + " is: " + factorial);
+    }
+    
+}
--- basics/functions.java
+++ basics/functions.java
@@ -6,6 +6,9 @@ public class functions {
     public static int add(int a, int b) {
         return a + b;
     }
+public static int subtract(int a, int b) {
+        return a - b;
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -14,8 +17,9 @@ public class functions {
         System.out.print("Enter second number: ");
         int b = sc.nextInt();
         int sum = add(a, b);
+        int difference = subtract(a, b);                    
         System.out.println("The sum of " + a + " and " + b + " is: " + sum);
-        
+        System.out.println("The difference of " + a + " and " + b + " is: " + difference);
     }
 
 }
